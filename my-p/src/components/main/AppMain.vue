6f5811242b4d136715a4fbd483a3825f80d01819
zj <1052843688@qq.com>
2017-12-26 22:11:33 +0800

    zj create a cli of project

---
+++ my-p/src/components/main/AppMain.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+	<header>
+      千磨万击还渐进，任尔东西南北风
+	</header>
+	<section>
+          <ul>
+          	<li></li>
+          	<li></li>
+          	<li></li>
+          </ul>		
+	</section>
+	<nav>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</nav>
+	<div class="list">
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<footer>
+		<ul><li></li><li></li></ul>
+	</footer>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"AppMain",
+	}
+</script>
+
+<style lang="scss">
+   $basecolor:red;
+    body{
+    	background:$basecolor;
+    }
+    header{
+    	width:100%;
+    	height:0.44rem;
+    	background:#999;
+    }
+</style>
