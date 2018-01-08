46f9a25e01f8966e6f8184ad18e9951685e4c8b8
glsnb <guoyu5212@163.com>
2018-01-08 21:35:44 +0800

    登录注册

--- my-p/src/components/sign/signIn.vue
+++ my-p/src/components/sign/signIn.vue
@@ -1,14 +1,46 @@
-<template lang="html">
+<template>
 	<div class="signIn">
-		signIn
+		
+		<div class="signIn-out">
+			<div class="logo"></div>
+			<p>欢迎来到集贤招聘</p>
+		</div>
+		
+		<div class="input">
+			<div class="one">
+				<p><i class="txt">sdf</i>
+					<i class="co"><input type="text"/></i>
+				</p>
+				<p><i class="txt">sdf</i>
+					<i class="co"><input type="text"/></i>
+					<p class="forget">忘记密码？</p>
+				</p>
+				<div class="signIn">登录</div>
+			</div>
+			
+		</div>
+		<div class="footer">
+			<div class="line">
+				<p class="other">其他社交网站登录</p>
+				
+			</div>
+			<div class="qq-out">
+				<div class="qq"></div>
+				<div class="qq"></div>
+				<div class="qq"></div>
+			</div>
+			
+		</div>
+		
+		
 	</div>
 </template>
-	
+
 <script>
 	export default {
-		name: 'signIn'
+		name:'signIn'
 	}
 </script>
-	
+
 <style>
 </style>
