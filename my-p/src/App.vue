7a478d5e61a1c070b0b48d67610f7f370123b45a
glsnb <guoyu5212@163.com>
2018-01-15 21:23:18 +0800

    init

--- my-p/src/App.vue
+++ my-p/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
   	
-    <signIn></signIn>
+    
    
   </div>
 </template>
 
 <script>
-import signIn from './components/sign/signIn.vue'
+
 export default {
   name: 'app',
 //components:{signIn}
